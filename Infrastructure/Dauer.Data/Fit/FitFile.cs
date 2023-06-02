2678c1ac5c7ec95dd547c70ccc4ca0a631ad6c81
Doug Slater <doug@slater.dev>
2023-06-02 13:52:01 -0400

    Optimization: Construct Recrods lazily. Fixes slow slider

--- Infrastructure/Dauer.Data/Fit/FitFile.cs
+++ Infrastructure/Dauer.Data/Fit/FitFile.cs
@@ -11,7 +11,19 @@ namespace Dauer.Data.Fit
 
     public List<SessionMesg> Sessions => Get<SessionMesg>();
     public List<LapMesg> Laps => Get<LapMesg>();
-    public List<RecordMesg> Records => Get<RecordMesg>().Sorted(MessageExtensions.Sort);
+
+    private Lazy<List<RecordMesg>> records_;
+    public List<RecordMesg> Records => records_.Value;
+
+    public FitFile()
+    {
+      records_ = new(ComputeRecords);
+    }
+
+    /// <summary>
+    /// Expensive; 2000 records take ~0.3s in WASM
+    /// </summary>
+    private List<RecordMesg> ComputeRecords() => Get<RecordMesg>().Sorted(MessageExtensions.Sort);
 
     public List<T> Get<T>() where T : Mesg => Messages
       .Where(message => message.Num == MessageFactory.MesgNums[typeof(T)])
@@ -28,6 +40,7 @@ namespace Dauer.Data.Fit
         _ when x is MesgDefinitionEventArgs mea => new MesgDefinitionEventArgs(mea.mesgDef),
         _ => null
       }).Where(x => x is not null).ToList(),
+      records_ = records_,
     };
   }
 }
