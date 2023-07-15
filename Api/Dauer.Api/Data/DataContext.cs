54273158adfd6bde537bc425a3ae8e6acd7982df
Doug Slater <doug@slater.dev>
2023-07-15 10:34:14 -0400

    Meant for previous commit

---
+++ Api/Dauer.Api/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Dauer.Api.Data;
+
+public class DataContext : DbContext
+{
+  protected readonly IConfiguration Configuration;
+
+  public DbSet<User> Users { get; set; }
+
+  public DataContext(IConfiguration configuration)
+  {
+    Configuration = configuration;
+  }
+
+  protected override void OnConfiguring(DbContextOptionsBuilder options)
+  {
+    options.UseNpgsql(Configuration.GetConnectionString("Default"));
+  }
+}
+
+public class User
+{
+  public string? Id { get; set; }
+}
