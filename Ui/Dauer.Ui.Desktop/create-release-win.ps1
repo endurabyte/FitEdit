1860ae2a990b456b57f41a08fa6d5ebafacab563
Doug Slater <doug@slater.dev>
2023-07-18 15:56:18 -0400

    self-sign windows app
    Former-commit-id: 1529ceb011ec35f882c38bfb853f741e6311a591

--- Ui/Dauer.Ui.Desktop/create-release-win.ps1
+++ Ui/Dauer.Ui.Desktop/create-release-win.ps1
@@ -2,7 +2,7 @@ dotnet tool install -g csq --prerelease
 
 dotnet publish Dauer.Ui.Desktop.csproj --configuration Release --runtime win-x64 --framework net7.0 --output "./bin/Release/net7.0/publish/win-x64/" --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=false
 
-csq pack --xplat=win --packId "FitEdit" --packAuthors "Doug Slater" --packVersion "1.0.3" --packDirectory "./bin/Release/net7.0/publish/win-x64" --icon "../Dauer.Ui/Assets/logo.ico" --mainExe "FitEdit.exe" --releaseDir "./releases/win-x64"
+csq pack --xplat=win --packId "FitEdit" --packAuthors "Doug Slater" --packVersion "1.0.3" --packDirectory "./bin/Release/net7.0/publish/win-x64" --icon "../Dauer.Ui/Assets/logo.ico" --mainExe "FitEdit.exe" --releaseDir "./releases/win-x64" --signParams="/n fitedit-selfSigned /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com"
 
 # Sync with s3
 pushd
