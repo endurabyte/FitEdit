7d34cf55cc9c5e15e5e89f0d32fb734525c83f12
Doug Slater <doug@slater.dev>
2023-08-01 04:04:16 -0400

    Sign with real cert

--- Ui/Dauer.Ui.Desktop/create-release-win.ps1
+++ Ui/Dauer.Ui.Desktop/create-release-win.ps1
@@ -8,25 +8,23 @@ $configuration = "Release"
 $rid = "win-x64"
 $authors = "EnduraByte LLC"
 $packId = "FitEdit"
-$certTmpPath = "cert.tmp.pfx"
-$certSubject = "fitedit-SelfSigned"
+$certTmpPath = "cert.tmp.crt"
+$certKey = $env:FITEDIT_WINDOWS_CODE_SIGN_KEY
 
 pushd $PSScriptRoot
 
 echo "Creating $certTmpPath..."
 & ./Decode-FromBase64.ps1 $env:FITEDIT_WINDOWS_CODE_SIGN_CERTIFICATE $certTmpPath
-
-echo "Importing $certTmpPath into certificate store..."
-$pass = ConvertTo-SecureString -String $env:FITEDIT_WINDOWS_CODE_SIGN_CERTIFICATE_PASSWORD -Force -AsPlainText
-Import-PfxCertificate -FilePath $certTmpPath -CertStoreLocation Cert:\CurrentUser\My -Password $pass
-Remove-Item -Path $certTmpPath
+$certTmpPath = "$PSScriptRoot/$certTmpPath"
 
 echo "Publishing..."
 dotnet publish Dauer.Ui.Desktop.csproj --configuration Release --runtime $rid --framework $framework --output "./bin/Release/$framework/publish/$rid/" --self-contained true -p:PublishSingleFile=true -p:PublishTrimmed=false
 
 echo "Packing..."
 dotnet tool install -g csq --prerelease
-csq pack --xplat=win --packId "FitEdit" --packAuthors $authors --packVersion $version --packDirectory "./bin/Release/$framework/publish/$rid" --icon "../Dauer.Ui/Assets/logo.ico" --mainExe "FitEdit.exe" --releaseDir "./releases/$rid" --signParams="/n $certSubject /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com" --noDelta
+csq pack --xplat=win --packId "FitEdit" --packAuthors $authors --packVersion $version --packDirectory "./bin/Release/$framework/publish/$rid" --icon "../Dauer.Ui/Assets/logo.ico" --mainExe "FitEdit.exe" --releaseDir "./releases/$rid" --signParams="/f `"$certTmpPath`" /fd SHA256 /td SHA256 /tr http://timestamp.digicert.com /csp `"SafeNet Smart Card Key Storage Provider`" /k $certKey" --noDelta
+
+Remove-Item -Path $certTmpPath
 
 $doSync = [System.Boolean]::Parse($sync)
 if ($doSync -ne $true) {
