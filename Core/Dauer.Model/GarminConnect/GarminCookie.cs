50bebd8ddc9413f483870f8f92b2acf128cdecd3
Doug Slater <doug@slater.dev>
2023-10-03 16:09:18 -0400

    Meant for previous commit

---
+++ Core/Dauer.Model/GarminConnect/GarminCookie.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using System.Text.Json.Serialization;
+
+namespace Dauer.Model.GarminConnect;
+
+public class GarminCookie
+{
+  [JsonPropertyName("name")]
+  public string? Name { get; set; }
+  [JsonPropertyName("value")]
+  public string? Value { get; set; }
+  [JsonPropertyName("domain")]
+  public string? Domain { get; set; }
+  [JsonPropertyName("path")]
+  public string? Path { get; set; }
+}
