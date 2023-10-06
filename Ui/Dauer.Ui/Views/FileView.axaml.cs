4d8285ad2310f5b53855715b00451e8f43235083
Doug Slater <doug@slater.dev>
2023-10-06 10:19:16 -0400

    Meant for commit a795b

--- Ui/Dauer.Ui/Views/FileView.axaml.cs
+++ Ui/Dauer.Ui/Views/FileView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Platform.Storage;
+using Dauer.Model;
 using Dauer.Ui.ViewModels;
 using ReactiveUI;
 
@@ -14,6 +17,10 @@ public partial class FileView : UserControl
       if (FileListBox.Scroll is not ScrollViewer sv) { return; }
       sv.ScrollChanged += HandleScrollChanged;
     });
+
+    AddHandler(DragDrop.DragEnterEvent, HandleDragEnter);
+    AddHandler(DragDrop.DragLeaveEvent, HandleDragLeave);
+    AddHandler(DragDrop.DropEvent, HandleDrop);
   }
 
   private void HandleScrollChanged(object? sender, ScrollChangedEventArgs _)
@@ -25,5 +32,32 @@ public partial class FileView : UserControl
     if (maximumScroll <= 0) { return; }
 
     vm.ScrollPercent = sv.Offset.Y / maximumScroll * 100;
+  }
+
+  private void HandleDragEnter(object? sender, DragEventArgs e)
+  {
+    if (DataContext is not IFileViewModel vm) { return; }
+    vm.IsDragActive = true;
+  }
+
+  private void HandleDragLeave(object? sender, DragEventArgs e)
+  {
+    if (DataContext is not IFileViewModel vm) { return; }
+    vm.IsDragActive = false;
+  }
+
+  private void HandleDrop(object? sender, DragEventArgs e)
+  {
+    if (DataContext is not IFileViewModel vm) { return; }
+    vm.IsDragActive = false;
+
+    if (e.Data.GetFiles() is { } files)
+    {
+      foreach (IStorageItem item in files)
+      {
+        if (item is not IStorageFile file) { continue; }
+        vm.HandleFileDropped(file);
+      }
+    }
   }
 }
