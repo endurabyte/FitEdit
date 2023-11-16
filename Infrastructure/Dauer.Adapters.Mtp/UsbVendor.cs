81aaf43f4743f5b6d928e1409c2591869531927a
Doug Slater <doug@slater.dev>
2023-11-16 16:13:16 -0500

    Add UsbVendor

---
+++ Infrastructure/Dauer.Adapters.Mtp/UsbVendor.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Dauer.Adapters.Mtp;
+
+public static class UsbVendor
+{
+  private static readonly Dictionary<string, uint> supportedVendors_ = new()
+  {
+    ["GARMIN"] = 0x091E,
+  };
+
+  public static bool IsSupported(string vendorId)
+  {
+    // Linux and Windows report the vendor ID in hex.
+    // macOS reports in decimal
+    // So we check both, i.e Garmin 0x091E == 2334
+
+    return supportedVendors_.Values.Any(id =>
+    {
+      bool isSupported = uint.TryParse(vendorId, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out uint hex) && hex == id;
+      isSupported |= uint.TryParse(vendorId, out uint dec) && dec == id;
+
+      return isSupported;
+    });
+  }
+}
