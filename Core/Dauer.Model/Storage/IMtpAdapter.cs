490c217fc444567ccdd366954ae11213865a0fcd
Doug Slater <doug@slater.dev>
2023-11-16 19:22:19 -0500

    Prompt user to download files

--- Core/Dauer.Model/Storage/IMtpAdapter.cs
+++ Core/Dauer.Model/Storage/IMtpAdapter.cs
@@ -5,9 +5,11 @@ namespace Dauer.Model.Storage;
 public interface IMtpAdapter
 {
   void Scan();
+  void GetFiles(PortableDevice dev);
 }
 
 public class NullMtpAdapter : IMtpAdapter
 {
   public void Scan() { }
+  public void GetFiles(PortableDevice dev) { }
 }
