7d6e367c6fd8cbdc96c1c771aff95279712654c4
Doug Slater <doug@slater.dev>
2024-09-29 15:55:37 -0400

    Remove hex viewer

--- Ui/FitEdit.Ui/Views/RecordView.axaml.cs
+++ Ui/FitEdit.Ui/Views/RecordView.axaml.cs
@@ -11,28 +11,14 @@ public partial class RecordView : UserControl
   public RecordView()
   {
     InitializeComponent();
-    this.ObservableForProperty(x => x.DataContext).Subscribe(_ => HandleDataContextChanged());
+    this.ObservableForProperty(x => x.DataContext)
+      .Subscribe(_ => HandleDataContextChanged());
   }
 
   private void HandleDataContextChanged()
   {
     if (DataContext is not IRecordViewModel vm) { return; }
     vm_ = vm;
-
-    vm.ObservableForProperty(x => x.ShowHexData).Subscribe(_ => HandleHexSelectionChanged());
-    vm.ObservableForProperty(x => x.HexDataSelectionStart).Subscribe(_ => HandleHexSelectionChanged());
-  }
-
-  private void HandleHexSelectionChanged()
-  {
-    if (vm_ == null) { return; }
-
-    double perc = vm_.HexDataSelectionStart / (double)vm_.HexData.Length;
-    double height = HexScrollViewer.Extent.Height;
-    double newHeight = perc * height - HexScrollViewer.Viewport.Height / 2;
-
-    if (double.IsNaN(newHeight)) { return; }
-    HexScrollViewer.Offset = new(HexScrollViewer.Offset.X, newHeight);
 
   }
 }
